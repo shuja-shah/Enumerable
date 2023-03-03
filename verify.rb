8b9fa4d64cb3c6eb4daa5b7e32a7be43d3f7b38d
Jonah Wambua <djmrjay@gmail.com>
2023-03-03 16:10:16 +0300

    Test code

--- verify.rb
+++ verify.rb
@@ -1,1 +1,19 @@
-# Verify Your Enumerable Here
+require_relative 'enumerable'
+
+class List
+  include Enumberable
+  def initialize(list)
+    @list = list
+  end
+
+  def each(&list)
+    @list.each(&list)
+  end
+end
+
+## TESTS
+
+list = List.new([1, 2, 3, 4, 5])
+puts list.all? { |i| i < 6 } # => true
+puts list.any? { |i| i < 2 } # => true
+puts list.filter { |i| i < 3 } # => [1,2]
